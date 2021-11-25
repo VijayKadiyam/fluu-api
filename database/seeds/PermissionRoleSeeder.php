c3b751473b8ba755b38bc39025a21e3ce8ef8ff6
VijayKadiyam <freelancervijayk@gmail.com>
2021-11-25 13:10:40 +0530

    Permission changed

--- database/seeds/PermissionRoleSeeder.php
+++ database/seeds/PermissionRoleSeeder.php
@@ -25,6 +25,19 @@ class PermissionRoleSeeder extends Seeder
     $role->assignpermission(9);
     $role->assignpermission(10);
 
+    // Main Admin Permissions
+    $role = Role::find(2);
+    $role->assignpermission(1);
+    $role->assignpermission(2);
+    $role->assignpermission(3);
+    $role->assignpermission(4);
+    $role->assignpermission(5);
+    $role->assignpermission(6);
+    $role->assignpermission(7);
+    $role->assignpermission(8);
+    $role->assignpermission(9);
+    $role->assignpermission(10);
+
     // Admin Permissions
     $role = Role::find(2);
     $role->assignpermission(1);
